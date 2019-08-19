7bda4372bf34f42e10a379ad59e0221ab5fb0465
Kaléu Caminha <kaleu@vios.com.br>
2019-08-19 17:22:09 -0300

    Abstracting random points generator for cluster example

--- cluster-example/app.js
+++ cluster-example/app.js
@@ -1,20 +1,14 @@
-import Feature from 'ol/Feature.js';
 import Map from 'ol/Map.js';
 import View from 'ol/View.js';
-import Point from 'ol/geom/Point.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import {Cluster, OSM, Vector as VectorSource} from 'ol/source.js';
 import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style.js';
+import FeatureGenerator from "./FeatureGenerator";
+
+const featureGenerator = new FeatureGenerator();
+let features = featureGenerator.generatePointsToMercatorProjection(10000);
 
 var distance = document.getElementById('distance');
-
-var count = 20000;
-var features = new Array(count);
-var e = 4500000;
-for (var i = 0; i < count; ++i) {
-  var coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
-  features[i] = new Feature(new Point(coordinates));
-}
 
 var source = new VectorSource({
   features: features
